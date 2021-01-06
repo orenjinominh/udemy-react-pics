e6559ddd88bbfe65578068957425bb1966e16269
Minh <minhvn29@gmail.com>
2021-01-05 16:01:53 -0800

    Hook up Unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import axios from 'axios'; 
 import SearchBar from './SearchBar';
+require ('dotenv').config();
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+const API_KEY = process.env.REACT_APP_UNSPLASH;
+
+class App extends React.Component {
+
+
+
+  onSearchSubmit(term) {
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: {query: term},
+      headers: {
+        Authorization: API_KEY
+      }
+    });
+  }
+
+  render () {
+    return (
+      <div className="ui container" style={{marginTop: '10px'}}>
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+      </div>
+    );
+  }
+
+}
+
 
 export default App;
